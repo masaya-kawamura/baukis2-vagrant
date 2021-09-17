0b11c215d31afe8a16d2d1b16c401fe068bb7fa8
masaya-kawamura <lucky.number0128@gmail.com>
2021-09-17 19:38:25 +0900

    [add]職員新規登録画面の追加

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -2,4 +2,15 @@ class Admin::StaffMembersController < Admin::Base
   def index
     @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
   end
+
+  def show
+    staff_member = StaffMember.find(params[:id])
+    # 配列の要素からリダ入れクション先が推定される。
+    # edit_admin_staff_member_path(id: staff_member.id)
+    redirect_to [ :edit, :admin, staff_member ]
+  end
+
+  def new
+    @staff_member = StaffMember.new
+  end
 end
