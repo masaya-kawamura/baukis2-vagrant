1d80abe251a67c99c5a6f6d9204dc8e8a799176b
masaya-kawamura <lucky.number0128@gmail.com>
2021-10-07 06:49:10 +0900

    [add]職員アカウント編集画面にバリデーションエラー機能を追加

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,6 +1,12 @@
 class StaffMember < ApplicationRecord
   has_many :events, class_name: "StaffEvent", dependent: :destroy
 
+  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
+
+  validates :family_name, :given_name, presence: true
+  validates :family_name_kana, :given_name_kana, presence: true,
+    format: { with: KATAKANA_REGEXP, allow_blank: true }
+
   def password=(raw_password)
     #パスワードが文字列な(kind_of?(String))らハッシュ化、それ以外ならnilを返す
     if raw_password.kind_of?(String)
